4fe97d14647da107eee17576663864a80dc565c7
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-11 15:25:50 +0200

    remove import navbar from contactus

--- master/src/Pages/ContactUS.jsx
+++ master/src/Pages/ContactUS.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
 
 const Contacts = () => {
   return (
     <div>
 
         <h2>Contacts</h2>
-        
+
     </div>
   )
 }
