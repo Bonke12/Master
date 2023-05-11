2c3c36961218bb82027e330ac8522ac0637eec2b
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-11 14:03:51 +0200

    add import contactUS from Navbar

---
+++ master/src/Pages/ContactUS.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const ContactUS = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h2>ContactUS</h2>
+    </div>
+  )
+}
+
+export default ContactUS
