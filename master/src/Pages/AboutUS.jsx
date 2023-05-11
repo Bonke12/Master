acba014e65b8330372fc47af45ff232db34ce54e
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-11 14:07:57 +0200

    imort aboutus from Navbar

---
+++ master/src/Pages/AboutUS.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const AboutUS = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h2>AboutUS</h2>
+    </div>
+  )
+}
+
+export default AboutUS
