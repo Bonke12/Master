448ab6d43124f0aa972df9b6f3c262e4dfb07ae9
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-12 09:40:12 +0200

    import slide from app

--- master/src/App.jsx
+++ master/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
+import Slide from './Components/Slide'
 
 const App = () => {
   return (
     <div>
         <Navbar/>
         <Home/>
+        <Slide/>
     </div>
   )
 }
