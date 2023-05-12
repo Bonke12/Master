9bc730bdd366c7bbabbafd0977e761417d2468e1
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-12 13:13:50 +0200

    change title

--- master/src/Components/Slide.jsx
+++ master/src/Components/Slide.jsx
@@ -3,37 +3,27 @@ import Carousel from 'react-bootstrap/Carousel';
 function IndividualIntervalsExample() {
     return (
         <Carousel>
-            <Carousel.Item interval={1000}>
-                <img
-                    className="d-block w-100"
-                    src=""
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>Products to sell</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-                <img
-                    className="d-block w-100"
-                    src=""
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3>Buy online things</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src=""
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Take your products to send home</h3>
-
-                </Carousel.Caption>
-            </Carousel.Item>
+            <div id="carouselExampleFade" class="carousel slide carousel-fade">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://images.thedirect.com/media/article_full/marvel-studios-producer-explains-the-restrictions-of-mcu-live-action-projects.jpg" class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/08/07/15/marvel.jpg" class="d-block w-100" alt="..." />
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="https://www.experienceperception.com/wp-content/uploads/Case-Studies/Marvel-Studios-Rebrand/marvel-studios-logo-rebrand-8.jpg" />
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </Carousel>
     );
 }
