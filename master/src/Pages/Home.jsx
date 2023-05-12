befd8adb8010cabe78d5e6e394c22b2fa20aed4b
Bonke12 <bonkenyamalala39@gmail.com>
2023-05-12 09:38:30 +0200

    import slide from home

--- master/src/Pages/Home.jsx
+++ master/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Slide from '../Components/Slide'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Slide/>
     </div>
   )
 }
